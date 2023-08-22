da0704dd1285c804edd4120c7a5b72b5278392d8
Allen Robel <arobel@cisco.com>
2023-08-22 10:33:44 -1000

    refactor

--- dcnm/test_fabric_params.py
+++ dcnm/test_fabric_params.py
@@ -1,19 +1,40 @@
 #!/usr/bin/env python
-import sys
 from ansible_collections.cisco.dcnm.plugins.module_utils.fabric.fabric import (
     VerifyFabricParams,
 )
+SHOW_PAYLOAD = False
 
-config = {}
-config["fabric_name"] = "foo"
-config["bgp_as"] = "65000.869"
-config["auto_symmetric_vrf_lite"] = True
-config["vrf_lite_autoconfig"] = 0
-verify = VerifyFabricParams()
-verify.config = config
-verify.state = "merged"
-verify.validate_config()
-if verify.result == False:
-    print(f"result {verify.result}, message {verify.msg}")
-    sys.exit(1)
-print(f"result {verify.result}, message {verify.msg}, payload {verify.payload}")
+def print_result(result, msg, payload):
+    _payload = ""
+    if SHOW_PAYLOAD:
+        _payload = f", payload {payload}"
+    print(f"result {result}, message {msg}{_payload}")
+
+def test_bfd():
+    config = {}
+    config["fabric_name"] = "foo"
+    config["bgp_as"] = "65000.869"
+    config["bfd_auth_key_id"] = "100"
+    config["bfd_enable"] = True
+    config["bfd_auth_enable"] = True
+    config["bfd_auth_key"] = "asadflkajsdff"
+    verify = VerifyFabricParams()
+    verify.config = config
+    verify.state = "merged"
+    verify.validate_config()
+    print_result(verify.result, verify.msg, verify.payload)
+
+def test_vrf_lite():
+    config = {}
+    config["fabric_name"] = "foo"
+    config["bgp_as"] = "65000.869"
+    config["auto_symmetric_vrf_lite"] = True
+    config["vrf_lite_autoconfig"] = 0
+    verify = VerifyFabricParams()
+    verify.config = config
+    verify.state = "merged"
+    verify.validate_config()
+    print_result(verify.result, verify.msg, verify.payload)
+
+test_bfd()
+test_vrf_lite()
