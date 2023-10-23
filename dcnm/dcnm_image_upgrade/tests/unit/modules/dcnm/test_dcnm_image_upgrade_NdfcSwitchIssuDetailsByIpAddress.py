99e25deb9b418824dfed1f5e0385cc09c797b540
Allen Robel <arobel@cisco.com>
2023-10-23 07:03:03 -1000

    Rename test file to subclass, commit initial tests
    We'll test each subclass of NdfcSwitchIssuDetails separately using common response data.
    
    This initial commit is a partial set of tests for subclass NdfcSwitchIssuDetailsByIpAddress.

--- dcnm/dcnm_image_upgrade/tests/unit/modules/dcnm/test_dcnm_image_upgrade_NdfcSwitchIssuDetailsByIpAddress.py
+++ dcnm/dcnm_image_upgrade/tests/unit/modules/dcnm/test_dcnm_image_upgrade_NdfcSwitchIssuDetailsByIpAddress.py
@@ -1,5 +1,5 @@
 from dcnm_image_upgrade.dcnm_image_upgrade import (
-    NdfcSwitchIssuDetails, NdfcSwitchIssuDetailsByIpAddress, NdfcAnsibleImageUpgradeCommon
+    NdfcSwitchIssuDetailsByIpAddress
 )
 from dcnm_image_upgrade.tests.unit.modules.dcnm.fixture import load_fixture
 from ansible_collections.ansible.netcommon.tests.unit.modules.utils import (
@@ -12,6 +12,8 @@ from typing import Any, Dict
 ndfc_version: 12
 description: Verify functionality of class NdfcSwitchIssuDetails
 """
+# Here, we are using the superclass name, since we are sharing the
+# same response file across all subclasses.
 class_name = "NdfcSwitchIssuDetails"
 response_file = f"dcnm_image_upgrade_responses_{class_name}"
 
@@ -28,8 +30,7 @@ def response_data(key: str) -> Dict[str, str]:
 
 @pytest.fixture
 def module():
-    return NdfcSwitchIssuDetailsByIpAddress(NdfcSwitchIssuDetails(NdfcAnsibleImageUpgradeCommon(MockAnsibleModule)))
-    # return NdfcSwitchIssuDetails(MockAnsibleModule)
+    return NdfcSwitchIssuDetailsByIpAddress(MockAnsibleModule)
 
 
 def test_init_properties(module) -> None:
@@ -64,6 +65,29 @@ def test_refresh_return_code_200(monkeypatch, module) -> None:
     )
     module.refresh()
     assert isinstance(module.ndfc_response, dict)
+    assert isinstance(module.ndfc_data, list)
+
+def test_properties_are_set_to_expected_values(monkeypatch, module) -> None:
+    """
+    Properties are set based on ip_address setter value.
+    endpoint: .../api/v1/imagemanagement/rest/packagemgnt/issu
+    """
+    key = "packagemgnt_issu_get_return_code_200_many_switch"
+
+    def mock_dcnm_send(*args, **kwargs) -> Dict[str, Any]:
+        print(f"mock_dcnm_send: {response_data(key)}")
+        return response_data(key)
+
+    monkeypatch.setattr("dcnm_image_upgrade.dcnm_image_upgrade.dcnm_send",
+        mock_dcnm_send
+    )
+    module.refresh()
+    module.ip_address = "172.22.150.102"
+    assert module.serial_number == "FDO21120U5D"
+    # change ip_address to a different switch, expect different information
+    module.ip_address = "172.22.150.108"
+    assert module.serial_number == "FDO2112189M"
+    assert module.device_name == "cvd-2313-leaf"
 
 def test_ndfc_result_return_code_200(monkeypatch, module) -> None:
     """
@@ -87,8 +111,7 @@ def test_ndfc_result_return_code_200(monkeypatch, module) -> None:
 def test_ndfc_result_return_code_404(monkeypatch, module) -> None:
     """
     fail_json is called on 404 response from malformed endpoint.
-    endpoint: .../api/v1/imagemanagement/rest/packagemgnt/issuu
-    TODO: This does not raise.  Look into this later...
+    endpoint: .../api/v1/imagemanagement/rest/policymgnt/policiess
     """
     key = "packagemgnt_issu_get_return_code_404"
 
@@ -96,14 +119,8 @@ def test_ndfc_result_return_code_404(monkeypatch, module) -> None:
         print(f"mock_dcnm_send: {response_data(key)}")
         return response_data(key)
 
-    def mock_fail_json(msg) -> AnsibleFailJson:
-        raise AnsibleFailJson(msg)
-
     monkeypatch.setattr("dcnm_image_upgrade.dcnm_image_upgrade.dcnm_send",
         mock_dcnm_send
-    )
-    monkeypatch.setattr("dcnm_image_upgrade.dcnm_image_upgrade.AnsibleModule.fail_json",
-        mock_fail_json
     )
     error_message = "Bad result when retriving switch information from NDFC"
     with pytest.raises(AnsibleFailJson, match=error_message):
@@ -123,18 +140,18 @@ def test_ndfc_result_return_code_200_empty_data(monkeypatch, module) -> None:
     monkeypatch.setattr("dcnm_image_upgrade.dcnm_image_upgrade.dcnm_send",
         mock_dcnm_send
     )
-    error_message = "NdfcSwitchIssuDetails.refresh: Bad response when retrieving "
-    error_message += "image policy information from NDFC."
+    error_message = "NdfcSwitchIssuDetailsByIpAddress.refresh: "
+    error_message += "NDFC has no switch ISSU information."
     with pytest.raises(AnsibleFailJson, match=error_message):
         module.refresh()
 
-def test_ndfc_result_return_code_200_ndfc_has_no_defined_image_policies(monkeypatch, module) -> None:
+def test_ndfc_result_return_code_200_ndfc_switch_issu_info_length_0(monkeypatch, module) -> None:
     """
     fail_json is called on 200 response with DATA.lastOperDataObject length 0.
     endpoint: .../api/v1/imagemanagement/rest/policymgnt/policiess
     """
-    key = "policymgnt_policies_get_return_code_200"
-    key += "_ndfc_has_no_defined_image_policies"
+    key = "packagemgnt_issu_get_return_code_200"
+    key += "_ndfc_switch_issu_info_length_0"
 
     def mock_dcnm_send(*args, **kwargs) -> Dict[str, Any]:
         print(f"mock_dcnm_send: {response_data(key)}")
@@ -143,61 +160,7 @@ def test_ndfc_result_return_code_200_ndfc_has_no_defined_image_policies(monkeypa
     monkeypatch.setattr("dcnm_image_upgrade.dcnm_image_upgrade.dcnm_send",
         mock_dcnm_send
     )
-    error_message = "NdfcSwitchIssuDetails.refresh: "
-    error_message += "NDFC has no defined image policies."
+    error_message = "NdfcSwitchIssuDetailsByIpAddress.refresh: "
+    error_message += "NDFC has no switch ISSU information."
     with pytest.raises(AnsibleFailJson, match=error_message):
         module.refresh()
-
-def test_policy_name_not_found(monkeypatch, module) -> None:
-    """
-    fail_json() is called if response does not contain policy_name.
-    i.e. image policy with name FOO has not yet been created on NDFC.
-    """
-    key = "policymgnt_policies_get_return_code_200"
-
-    def mock_dcnm_send(*args, **kwargs) -> Dict[str, Any]:
-        print(f"mock_dcnm_send: {response_data(key)}")
-        return response_data(key)
-
-    monkeypatch.setattr("dcnm_image_upgrade.dcnm_image_upgrade.dcnm_send",
-        mock_dcnm_send
-    )
-    module.refresh()
-    module.policy_name = "FOO"
-    error_message = "NdfcSwitchIssuDetails._get: "
-    error_message += "policy_name FOO is not defined in NDFC"
-    with pytest.raises(AnsibleFailJson, match=error_message):
-        module.policy_type == "PLATFORM"
-
-def test_get_with_policy_name_None(module) -> None:
-    """
-    fail_json is called when _get() is called prior to setting policy_name.
-    """
-    error_message = "NdfcSwitchIssuDetails._get: instance.policy_name must be "
-    error_message += "set before accessing property imageName."
-    with pytest.raises(AnsibleFailJson, match=error_message):
-        module._get("imageName")
-
-def test_ndfc_result_return_code_200_policy_name_missing_in_response(monkeypatch, module) -> None:
-    """
-    fail_json is called on 200 response with missing policyName key.
-    endpoint: .../api/v1/imagemanagement/rest/policymgnt/policiess
-
-    NOTE: This is to cover a check in NdfcSwitchIssuDetails.refresh() for a scenario that should never happen.
-    TODO: Consider removing this check, and this testcase.
-    """
-    key = "policymgnt_policies_get_return_code_200"
-    key += "_policyName_missing_in_response"
-
-    def mock_dcnm_send(*args, **kwargs) -> Dict[str, Any]:
-        print(f"mock_dcnm_send: {response_data(key)}")
-        return response_data(key)
-
-    monkeypatch.setattr("dcnm_image_upgrade.dcnm_image_upgrade.dcnm_send",
-        mock_dcnm_send
-    )
-    error_message = "NdfcSwitchIssuDetails.refresh: "
-    error_message += "Cannot parse NDFC policy information"
-    with pytest.raises(AnsibleFailJson, match=error_message):
-        module.refresh()
-
