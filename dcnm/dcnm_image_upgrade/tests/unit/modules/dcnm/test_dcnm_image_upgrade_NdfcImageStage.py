b315f3e57f58a2a6d2359980c1ce6841e195e3de
Allen Robel <arobel@cisco.com>
2023-10-24 13:52:20 -1000

    Add more tests

--- dcnm/dcnm_image_upgrade/tests/unit/modules/dcnm/test_dcnm_image_upgrade_NdfcImageStage.py
+++ dcnm/dcnm_image_upgrade/tests/unit/modules/dcnm/test_dcnm_image_upgrade_NdfcImageStage.py
@@ -3,15 +3,15 @@ ndfc_version: 12
 description: Verify functionality of NdfcImageStage
 """
 
-from dcnm_image_upgrade.dcnm_image_upgrade import (
-    NdfcSwitchIssuDetailsBySerialNumber, NdfcImageStage, NdfcVersion
-)
-from dcnm_image_upgrade.tests.unit.modules.dcnm.fixture import load_fixture
-from ansible_collections.ansible.netcommon.tests.unit.modules.utils import (
-    AnsibleFailJson,
-)
-import pytest
 from typing import Any, Dict
+
+import pytest
+from ansible_collections.ansible.netcommon.tests.unit.modules.utils import \
+    AnsibleFailJson
+from dcnm_image_upgrade.dcnm_image_upgrade import (
+    NdfcImageStage, NdfcSwitchIssuDetailsBySerialNumber, NdfcVersion)
+from dcnm_image_upgrade.tests.unit.modules.dcnm.fixture import load_fixture
+
 
 def response_data_issu_details(key: str) -> Dict[str, str]:
     response_file = f"dcnm_image_upgrade_responses_NdfcSwitchIssuDetails"
@@ -19,11 +19,13 @@ def response_data_issu_details(key: str) -> Dict[str, str]:
     print(f"response_data_issu_details: {key} : {response}")
     return response
 
+
 def response_data_ndfc_version(key: str) -> Dict[str, str]:
     response_file = f"dcnm_image_upgrade_responses_NdfcVersion"
     response = load_fixture(response_file).get(key)
     print(f"response_data_ndfc_version: {key} : {response}")
     return response
+
 
 def response_data_ndfc_image_stage(key: str) -> Dict[str, str]:
     response_file = f"dcnm_image_upgrade_responses_NdfcImageStage"
@@ -38,13 +40,16 @@ class MockAnsibleModule:
     def fail_json(msg) -> AnsibleFailJson:
         raise AnsibleFailJson(msg)
 
+
 @pytest.fixture
 def module():
     return NdfcImageStage(MockAnsibleModule)
 
+
 @pytest.fixture
 def mock_issu_details() -> NdfcSwitchIssuDetailsBySerialNumber:
     return NdfcSwitchIssuDetailsBySerialNumber(MockAnsibleModule)
+
 
 def test_init_properties(module) -> None:
     """
@@ -59,11 +64,8 @@ def test_init_properties(module) -> None:
     assert module.properties.get("check_interval") == 10
     assert module.properties.get("check_timeout") == 1800
 
-def test_prune_serial_numbers(
-        monkeypatch,
-        module,
-        mock_issu_details
-    ) -> None:
+
+def test_prune_serial_numbers(monkeypatch, module, mock_issu_details) -> None:
     """
     prune_serial_numbers removes serial numbers from the list for which
     imageStaged == "Success" (TODO: AND policy == <target_policy>)
@@ -78,11 +80,14 @@ def test_prune_serial_numbers(
     1. module.serial_numbers == ["FDO2112189M", "FDO211218AX", "FDO211218B5"]
     2. module.serial_numbers != ["FDO211218FV", "FDO211218GC"]
     """
+
     def mock_dcnm_send(*args, **kwargs) -> Dict[str, Any]:
         key = "NdfcImageStage_test_prune_serial_numbers"
         return response_data_issu_details(key)
 
-    monkeypatch.setattr("dcnm_image_upgrade.dcnm_image_upgrade.dcnm_send", mock_dcnm_send)
+    monkeypatch.setattr(
+        "dcnm_image_upgrade.dcnm_image_upgrade.dcnm_send", mock_dcnm_send
+    )
 
     module.issu_detail = mock_issu_details
     module.serial_numbers = [
@@ -90,7 +95,7 @@ def test_prune_serial_numbers(
         "FDO211218AX",
         "FDO211218B5",
         "FDO211218FV",
-        "FDO211218GC"
+        "FDO211218GC",
     ]
     module.prune_serial_numbers()
     assert isinstance(module.serial_numbers, list)
@@ -101,11 +106,8 @@ def test_prune_serial_numbers(
     assert "FDO211218FV" not in module.serial_numbers
     assert "FDO211218GC" not in module.serial_numbers
 
-def test_validate_serial_numbers_failed(
-        monkeypatch,
-        module,
-        mock_issu_details
-    ) -> None:
+
+def test_validate_serial_numbers_failed(monkeypatch, module, mock_issu_details) -> None:
     """
     fail_json is called when imageStaged == "Failed".
 
@@ -114,11 +116,14 @@ def test_validate_serial_numbers_failed(
     FDO21120U5D should pass since imageStaged == "Success"
     FDO2112189M should fail since imageStaged == "Failed"
     """
+
     def mock_dcnm_send(*args, **kwargs) -> Dict[str, Any]:
         key = "NdfcImageStage_test_validate_serial_numbers"
         return response_data_issu_details(key)
 
-    monkeypatch.setattr("dcnm_image_upgrade.dcnm_image_upgrade.dcnm_send", mock_dcnm_send)
+    monkeypatch.setattr(
+        "dcnm_image_upgrade.dcnm_image_upgrade.dcnm_send", mock_dcnm_send
+    )
 
     module.issu_detail = mock_issu_details
     module.serial_numbers = ["FDO21120U5D", "FDO2112189M"]
@@ -128,3 +133,109 @@ def test_validate_serial_numbers_failed(
     with pytest.raises(AnsibleFailJson, match=error_message):
         module.validate_serial_numbers()
 
+def test_wait_for_image_stage_to_complete(monkeypatch, module, mock_issu_details) -> None:
+    """
+    _wait_for_image_stage_to_complete looks at the imageStaged status for each
+    serial number and waits for it to be "Success" or "Failed".
+    In the case where all serial numbers are "Success", the module returns.
+    In the case where any serial number is "Failed", the module calls fail_json.
+
+    Expectations:
+    1. module.serial_numbers_done should be a set()
+    2. module.serial_numbers_done should be length 2
+    3. module.serial_numbers_done should contain all serial numbers module.serial_numbers
+    4. The module should return without calling fail_json.
+    """
+
+    def mock_dcnm_send(*args, **kwargs) -> Dict[str, Any]:
+        key = "NdfcImageStage_test_wait_for_image_stage_to_complete"
+        return response_data_issu_details(key)
+
+    monkeypatch.setattr(
+        "dcnm_image_upgrade.dcnm_image_upgrade.dcnm_send", mock_dcnm_send
+    )
+
+    module.issu_detail = mock_issu_details
+    module.serial_numbers = [
+        "FDO21120U5D",
+        "FDO2112189M",
+    ]
+    module.check_interval = 0
+    module._wait_for_image_stage_to_complete()
+    assert isinstance(module.serial_numbers_done, set)
+    assert len(module.serial_numbers_done) == 2
+    assert "FDO21120U5D" in module.serial_numbers_done
+    assert "FDO2112189M" in module.serial_numbers_done
+
+def test_wait_for_image_stage_to_complete_fail_json(monkeypatch, module, mock_issu_details) -> None:
+    """
+    _wait_for_image_stage_to_complete looks at the imageStaged status for each
+    serial number and waits for it to be "Success" or "Failed".
+    In the case where all serial numbers are "Success", the module returns.
+    In the case where any serial number is "Failed", the module calls fail_json.
+
+    Expectations:
+    1. module.serial_numbers_done should be a set()
+    2. module.serial_numbers_done should be length 1
+    3. module.serial_numbers_done should contain FDO21120U5D
+    4. The module call fail_json on serial number FDO2112189M
+    """
+
+    def mock_dcnm_send(*args, **kwargs) -> Dict[str, Any]:
+        key = "NdfcImageStage_test_wait_for_image_stage_to_complete_fail_json"
+        return response_data_issu_details(key)
+
+    monkeypatch.setattr(
+        "dcnm_image_upgrade.dcnm_image_upgrade.dcnm_send", mock_dcnm_send
+    )
+
+    module.issu_detail = mock_issu_details
+    module.serial_numbers = [
+        "FDO21120U5D",
+        "FDO2112189M",
+    ]
+    module.check_interval = 0
+    with pytest.raises(AnsibleFailJson):
+        module._wait_for_image_stage_to_complete()
+    assert isinstance(module.serial_numbers_done, set)
+    assert len(module.serial_numbers_done) == 1
+    assert "FDO21120U5D" in module.serial_numbers_done
+    assert "FDO2112189M" not in module.serial_numbers_done
+
+def test_wait_for_current_actions_to_complete(monkeypatch, module, mock_issu_details) -> None:
+    """
+    _wait_for_current_actions_to_complete waits until staging, validation,
+    and upgrade actions are complete for all serial numbers.  It calls
+    NdfcSwitchIssuDetailsBySerialNumber.actions_in_progress() and expects
+    this to return False.  actions_in_progress() returns True until none of
+    the following keys has a value of "In-Progress":
+    
+    ["imageStaged", "upgrade", "validated"]
+
+    Expectations:
+    1.  module.serial_numbers_done should be a set()
+    2.  module.serial_numbers_done should be length 2
+    3.  module.serial_numbers_done should contain all serial numbers in
+        module.serial_numbers
+    4.  The function should return without calling fail_json.
+    """
+
+    def mock_dcnm_send(*args, **kwargs) -> Dict[str, Any]:
+        key = "NdfcImageStage_test_wait_for_current_actions_to_complete"
+        return response_data_issu_details(key)
+
+    monkeypatch.setattr(
+        "dcnm_image_upgrade.dcnm_image_upgrade.dcnm_send", mock_dcnm_send
+    )
+
+    module.issu_detail = mock_issu_details
+    module.serial_numbers = [
+        "FDO21120U5D",
+        "FDO2112189M",
+    ]
+    module.check_interval = 0
+    module._wait_for_current_actions_to_complete()
+    assert isinstance(module.serial_numbers_done, set)
+    assert len(module.serial_numbers_done) == 2
+    assert "FDO21120U5D" in module.serial_numbers_done
+    assert "FDO2112189M" in module.serial_numbers_done
